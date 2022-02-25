46c86fd28f4116d3dd93188b66c8ef54c6369c99
abidkhan03 <khansahb5566@gmail.com>
2022-02-25 16:29:50 +0500

    created match model

--- futsal/models.py
+++ futsal/models.py
@@ -9,3 +9,15 @@ class Team(models.Model):
     team_attended_by=models.CharField(max_length=25)
     member_created_at = models.DateField(default=datetime.now())
     member_updated_at = models.DateField(auto_now=True)
+
+
+
+class Match(models.Model):
+    time = models.DateTimeField(auto_now=True)
+    meridiem = models.CharField(max_length=5)
+    team1 = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='team1')
+    team2 = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='team2')
+    fee = models.IntegerField()
+    paid= models.IntegerField()
+    data = models.DateField(auto_now_add=True)
+
