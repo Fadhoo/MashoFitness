bdeeb65e896d25c16a6e10cb32b0665b7584033b
CodeWithEhtisham <shamikhan0347@gmail.com>
2022-03-24 18:09:32 +0500

    print category issue solved

--- theme/views.py
+++ theme/views.py
@@ -50,8 +50,8 @@ def login(request):
 def viewRecord(request):
     if request.method=="GET":
         bill=Bill.objects.filter(member_id=request.GET.get("cid")).select_related("member_id").select_related("fee_id").select_related("subscription_id").order_by("-id")
-        return render(request, "viewRecord.html", {"member_name":bill[0].member_id.member_name,"memberID":bill[0].member_id.id,
-                        'bill': bill,})
+        return render(request, "viewRecord.html", {"member_name":bill[0].member_id.member_name,"memberID":bill[0].member_id.id,'member_serial':bill[0].member_id.member_serial_no,"bill":bill})
+                        
 
 def smshistory(request):
     return render(request, 'smshistory.html')
