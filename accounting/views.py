c0258c996b3692adf50781ae0db08e5e716d156f
abidkhan03 <khansahb5566@gmail.com>
2022-03-28 11:54:33 +0500

    reports page integration

--- accounting/views.py
+++ accounting/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-
+from expenses.models import expensesData
+from django.db.models import Sum
 # Create your views here.
 def reports(request):
-    return render(request,"reports.html")
+    return render(request,"reports.html", {
+        'gym_expense':expensesData.objects.filter(expenses_for='Gym').aggregate(Sum('paid_amount'))['paid_amount__sum'],
+        'futsal_expense': expensesData.objects.filter(expenses_for='Futsal').aggregate(Sum('paid_amount'))['paid_amount__sum'],
+        'snooker_expense': expensesData.objects.filter(expenses_for='Snooker').aggregate(Sum('paid_amount'))['paid_amount__sum'],
+        'cafeteria_expense': expensesData.objects.filter(expenses_for='Cafeteria').aggregate(Sum('paid_amount'))['paid_amount__sum'],
+        'rental_expense': expensesData.objects.filter(expenses_for='Rental').aggregate(Sum('paid_amount'))['paid_amount__sum'],
+        'total_expenses': expensesData.objects.aggregate(Sum('paid_amount'))['paid_amount__sum']
+    })
 
 def rental(request):
     return render(request, "rental.html")
